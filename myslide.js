0fedea61b0a58fa611c74057ddc26fe6c74aa0fe
kaharkaboodi <erfankaharkaboodi@gmail.com>
2021-06-01 21:57:09 +0430

    first commit

---
+++ myslide.js
@@ -0,0 +1,6 @@
+function myjavab() {
+    var x = document.getElementById("f1");
+    var y = document.getElementById("a1").value;
+    var z = document.getElementById("a2").value;
+    document.getElementById("demo").innerHTML = Math.pow(y, z)
+}
